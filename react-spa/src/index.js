815fb71131b6bc7b34a7bd6dabb6d813cf0e5ef6
tommy <afitamoon@gmail.com>
2019-06-08 23:58:38 +0900

    init code

---
+++ react-spa/src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import { createMuiTheme, MuiThemeProvider  } from '@material-ui/core/styles';
+import red from '@material-ui/core/colors/red';
+import blue from '@material-ui/core/colors/blue';
+
+import Provider from 'react-redux';
+
+import Router from 'react-router-dom';
+
+
+// テーマカスタマイズ
+const theme = createMuiTheme({
+  palette: {
+    type: 'light',
+    primary: red,
+    secondary: blue,
+  },
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <MuiThemeProvider theme={theme}>
+      <Router>
+        <App />
+      </Router>
+    </MuiThemeProvider>
+  </Provicer>
+  , document.getElementById('root')
+);
+
