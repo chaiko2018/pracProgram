8735bd7185a7cf5f90dd82af04faa5c770d1f493
tommy <afitamoon@gmail.com>
2019-06-12 23:14:47 +0900

    fixd code

--- react-spa/src/index.js
+++ react-spa/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import createStore from 'redux';
+
 import { createMuiTheme, MuiThemeProvider  } from '@material-ui/core/styles';
 import red from '@material-ui/core/colors/red';
 import blue from '@material-ui/core/colors/blue';
@@ -10,6 +12,10 @@ import Provider from 'react-redux';
 
 import Router from 'react-router-dom';
 
+
+const store = createStore(
+  
+);
 
 // テーマカスタマイズ
 const theme = createMuiTheme({
