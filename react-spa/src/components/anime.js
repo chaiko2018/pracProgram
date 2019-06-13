b91b4e6e61e81d1bcb77a8f8bf4de913b7ac8525
tommy <afitamoon@gmail.com>
2019-06-14 00:05:34 +0900

    fixed

--- react-spa/src/components/anime.js
+++ react-spa/src/components/anime.js
@@ -1,22 +1,93 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
 import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import red from '@material-ui/core/colors/red';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
-
-const styles = {
+const useStyles = makeStyles(theme => ({
   card: {
-    width: 330,
+    width: 320,
     marginTop: 10,
-    marginBottom: 10,
+    marginBotom: 10,
   },
-  empty: {
-    width: 330,
-    height: 0,
-    margin: 0,
-    padding: 0,
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+
+}));
+
+function InfoCard() {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  function handleExpandClick() {
+    setExpanded(!expanded);
   }
+
+  return (
+    <Card className={classes.card}>
+      <CardHeader
+        avatar={
+          <Avatar className={classes.avatar}>
+            T
+          </Avatar>
+        }
+        action={
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Test Component"
+        subheader="June 7, 2019"
+      />
+      <CardContent>
+        <Typography component="p">
+          sample text 
+          Hi! What is your name!
+          Good Luck!
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <IconButton>
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton>
+          <ShareIcon />
+        </IconButton>
+        <IconButton
+          onClick={handleExpandClick}
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeoutt="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>
+            Chaiko アホ
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+};
+
+export default InfoCard;
